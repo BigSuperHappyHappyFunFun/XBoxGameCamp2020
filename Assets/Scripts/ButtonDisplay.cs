c422f608dca87fd38bb3616095c3f39c02dd4a22
ETdoFresh <ETdoFresh@gmail.com>
2020-10-21 21:11:45 -0500

    Update to 2020.1.10f1. Start initial Sample Scene with audio loop and random button.

---
+++ Assets/Scripts/ButtonDisplay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDisplay : MonoBehaviour
+{
+    public List<Sprite> sprites = new List<Sprite>();
+    public SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
---
+++ Assets/Scripts/LevelLoop/ChangeButtonOnLoop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeButtonOnLoop : MonoBehaviour
+{
+    void Start()
+    {
+        foreach (var audioSource in FindObjectsOfType<AudioSource>())
+            foreach (var buttonDisplay in FindObjectsOfType<ButtonDisplay>())
+                StartCoroutine(LoopAndChangeButton(audioSource, buttonDisplay));
+    }
+
+    IEnumerator LoopAndChangeButton(AudioSource audioSource, ButtonDisplay buttonDisplay)
+    {
+        while (true)
+        {
+            audioSource.Play();
+            buttonDisplay.spriteRenderer.sprite = ChooseRandomSprite(buttonDisplay.sprites);
+            yield return new WaitForSeconds(audioSource.clip.length);
+        }
+    }
+
+    Sprite ChooseRandomSprite(List<Sprite> sprites)
+    {
+        var randomI = Random.Range(0, sprites.Count);
+        return sprites[randomI];
+    }
+}
