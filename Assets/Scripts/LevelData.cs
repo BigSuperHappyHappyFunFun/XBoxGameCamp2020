ef791f4e6197dc16adc1a435dc2a79962441ec2c
ETdoFresh <ETdoFresh@gmail.com>
2020-10-23 21:45:13 -0500

    Add HTML5 and Menu scene. Add level1.csv  which determines when buttons show up on screen. Add songs.

---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelData : MonoBehaviour
+{
+    [System.Serializable]
+    public class ButtonRequest
+    {
+        public string name;
+        public string button;
+        public float start;
+        public float end;
+    }
+
+    public TextAsset level;
+    public List<ButtonRequest> buttonRequests;
+
+    private void Start()
+    {
+        var lines = level.text.Split('\n');
+        for (var i = 1; i < lines.Length; i++)
+        {
+            var buttonRequestStrings = lines[i].Split(',');
+
+            if (buttonRequestStrings.Length == 0)
+                continue;
+
+            var buttonRequest = new ButtonRequest();
+            buttonRequest.start = System.Convert.ToSingle(buttonRequestStrings[0]);
+            buttonRequest.end = System.Convert.ToSingle(buttonRequestStrings[1]);
+            buttonRequest.button = buttonRequestStrings[2];
+            buttonRequest.name = $"Button {buttonRequest.button} @ {buttonRequest.start} - {buttonRequest.end}";
+            buttonRequests.Add(buttonRequest);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelLoop/ChangeButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeButton : MonoBehaviour
+{
+    void Start()
+    {
+        foreach (var levelData in FindObjectsOfType<LevelData>())
+            foreach (var buttonDisplay in FindObjectsOfType<ButtonDisplay>())
+                StartCoroutine(DisplayButton(levelData, buttonDisplay));
+    }
+
+    IEnumerator DisplayButton(LevelData levelData, ButtonDisplay buttonDisplay)
+    {
+        var time = 0f;
+        for (var i = 0; i < levelData.buttonRequests.Count; i = i)
+        {
+            var buttonRequest = levelData.buttonRequests[i];
+            if (buttonRequest.start < time)
+            {
+                buttonDisplay.spriteRenderer.sprite = GetSprite(buttonDisplay, buttonRequest);
+                i++;
+            }
+            yield return null;
+            time += Time.deltaTime;
+        }
+    }
+
+    Sprite GetSprite(ButtonDisplay buttonDisplay, LevelData.ButtonRequest buttonRequest)
+    {
+        switch (buttonRequest.button.Trim())
+        {
+            case "A": return buttonDisplay.ButtonA;
+            case "B": return buttonDisplay.ButtonB;
+            case "X": return buttonDisplay.ButtonX;
+            case "Y": return buttonDisplay.ButtonY;
+            case "L": return buttonDisplay.ButtonL;
+            case "R": return buttonDisplay.ButtonR;
+            default: return null;
+        }
+    }
+}
