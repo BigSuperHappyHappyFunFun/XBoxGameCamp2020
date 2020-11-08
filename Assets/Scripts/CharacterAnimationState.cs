b7e20895f1d31bf1db60ed2bc79a56a0e569c436
ETdoFresh <ETdoFresh@gmail.com>
2020-11-07 21:28:40 -0600

    Add animation state to characters with debug scripts.

---
+++ Assets/Scripts/CharacterAnimationState.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationState : MonoBehaviour
+{
+    public Animator animator;
+    public float crossFadeTime = 0.5f;
+
+    public bool triggerSuccess = false;
+    public bool triggerFail = false;
+
+    private void OnValidate()
+    {
+        if (!animator) animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (triggerSuccess)
+        {
+            TriggerSuccess();
+            triggerSuccess = false;
+        }
+        if (triggerFail)
+        {
+            TriggerFail();
+            triggerFail = false;
+        }
+    }
+
+    public void TriggerSuccess()
+    {
+        animator.CrossFade("Success", crossFadeTime, 0, 0);
+    }
+
+    public void TriggerFail()
+    {
+        animator.CrossFade("Fail", crossFadeTime, 0, 0);
+    }
+}
---
+++ Assets/Scripts/DebugCharacterAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugCharacterAnimation : MonoBehaviour
+{
+    public CharacterAnimationState characterAnimationState;
+    public KeyCode success = KeyCode.Q;
+    public KeyCode fail = KeyCode.E;
+
+    private void OnValidate()
+    {
+        if (!characterAnimationState) characterAnimationState = GetComponent<CharacterAnimationState>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(success))
+            characterAnimationState.triggerSuccess = true;
+        if (Input.GetKeyDown(fail))
+            characterAnimationState.triggerFail = true;
+    }
+}
