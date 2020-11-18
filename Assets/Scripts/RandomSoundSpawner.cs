0d8e7fb7ff8b90b022ff7b28a1449faec180f288
ETdoFresh <ETdoFresh@gmail.com>
2020-11-18 16:12:51 -0600

    Add Harmonica and Trumpet sound as place holders.

---
+++ Assets/Scripts/RandomSoundSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSoundSpawner : MonoBehaviour
+{
+    public List<GameObject> soundPrefabs;
+
+    public void Spawn()
+    {
+        var randomI = Random.Range(0, soundPrefabs.Count);
+        Instantiate(soundPrefabs[randomI], transform);
+    }
+}
---
+++ Assets/Scripts/SoundSpawner.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SoundSpawner : MonoBehaviour
+{
+    public GameObject soundPrefab;
+
+    public void Spawn()
+    {
+        Instantiate(soundPrefab, transform);
+    }
+}
