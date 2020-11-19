121a676e721a7fb996a69d921b2bf77b05d5a583
ETdoFresh <ETdoFresh@gmail.com>
2020-11-19 08:55:53 -0600

    Add string cheese graphic.

---
+++ Assets/Scripts/EventOnEnable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EventOnEnable : MonoBehaviour
+{
+    public UnityEvent onEnabled;
+
+    private void OnEnable()
+    {
+        onEnabled.Invoke();
+    }
+}
