718d56f1ff76d07cbca4a61d1827930733dddce0
ETdoFresh <ETdoFresh@gmail.com>
2021-01-23 15:14:54 -0600

    Add version number on menu screen.

---
+++ Assets/Scripts/VersionText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class VersionText : MonoBehaviour
+{
+    public GameSettings gameSettings;
+    public TextMeshProUGUI textMesh;
+
+    private void OnValidate()
+    {
+        if (!textMesh) textMesh = GetComponent<TextMeshProUGUI>();
+        if (gameSettings && textMesh)
+            textMesh.text = $"v{gameSettings.Version}";
+    }
+
+    private void OnEnable() => OnValidate();
+}
