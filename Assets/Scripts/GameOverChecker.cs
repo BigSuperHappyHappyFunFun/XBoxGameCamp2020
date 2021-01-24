2ff12968b0604531075d27a38644ca3113688e31
ETdoFresh <ETdoFresh@gmail.com>
2021-01-24 10:01:35 -0600

    Add new art.

---
+++ Assets/Scripts/GameOverChecker.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverChecker : MonoBehaviour
+{
+    public int totalNotes = 12;
+    public int loseNotes = 3;
+}
