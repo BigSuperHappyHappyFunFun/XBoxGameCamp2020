0767f215d38f8df8d792241d731b291380165269
ETdoFresh <ETdoFresh@gmail.com>
2021-03-17 21:42:19 -0500

    Need to rework time math of when arrows move around, but should be almost done for the most part.

---
+++ Assets/Scripts/ShowSummaryScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ShowSummaryScreen : MonoBehaviour
+{
+    private ButtonRequestSpawn _buttonRequestSpawn;
+    private QueueBox _queueBox;
+
+    private void Update()
+    {
+        if (!_buttonRequestSpawn) _buttonRequestSpawn = FindObjectOfType<ButtonRequestSpawn>();
+        if (!_queueBox) _queueBox = FindObjectOfType<QueueBox>();
+        if (!_buttonRequestSpawn || !_queueBox) return;
+        var finishTime = _buttonRequestSpawn.levelData.buttonRequests.Max(x => x.time);
+        finishTime += _queueBox.displayDelay;
+        finishTime += _queueBox.secondsFromQueueBoxToCollin;
+        if (_buttonRequestSpawn.time >= finishTime)
+        {
+            FindObjectOfType<SummaryPanel>(true).gameObject.SetActive(true);
+            FindObjectOfType<HalfFadeOutPanel>(true).gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/SummaryPanel.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class SummaryPanel : MonoBehaviour
+{
+    
+}
